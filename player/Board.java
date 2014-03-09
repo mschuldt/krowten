4941880b81120b2a854e73d9a752e5ea55fe78b4
mschuldt <mbschuldt@gmail.com>
2014-03-09 16:56:35 -0700

    constructor

--- player/Board.java
+++ player/Board.java
@@ -3,6 +3,13 @@
 package player; //this with Piece.java don't seem like they belong here...
 
 public class Board{
-    
+    Piece[][] pieceArray;
+    public Board(){
+        pieceArray = new Piece[64][64];
+        pieceArray[0][0]   = null;
+        pieceArray[63][0]  = null;
+        pieceArray[0][63]  = null;
+        pieceArray[63][63] = null;
+    }
 }
 
