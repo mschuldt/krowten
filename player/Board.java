ecc0722248519311d23cc294bd413b5c95b01522
mschuldt <mbschuldt@gmail.com>
2014-03-09 17:25:44 -0700

    method: Board.move

--- player/Board.java
+++ player/Board.java
@@ -11,5 +11,24 @@ public class Board{
         pieceArray[0][63]  = null;
         pieceArray[63][63] = null;
     }
+    //? public/protected?
+    //This assumes that MOVE is valid
+    public void move(Move move, int color){
+        switch (move.moveKind){
+        case Move.ADD :
+            assert pieceArray[move.x1][move.y1] == null : "square is already full";
+            pieceArray[move.x1][move.y1] = new Piece(color, move.x1, move.y1);
+            break;
+        case Move.STEP :
+            assert pieceArray[move.x1][move.y1] == null : "square is already full";
+            assert pieceArray[move.x2][move.y2] != null : "square is empty";
+            pieceArray[move.x1][move.x1] = pieceArray[move.x2][move.x2];
+            pieceArray[move.x2][move.x2] = null;
+            break;
+        case Move.QUIT :
+            //TODO
+            break;
+        }
+    }
 }
 
