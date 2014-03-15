7c31528fd4618d1c5eeb3e65e58994a8a09c0432
mschuldt <mbschuldt@gmail.com>
2014-03-15 02:39:01 -0700

    PieceLists how extends Alist instead of List

--- player/PieceList.java
+++ player/PieceList.java
@@ -1,13 +1,16 @@
 package player;
 
-public class PieceList extends List<Piece>{
+public class PieceList extends AList<Piece>{
     //add a piece to the back of this list if it is not null and not an edge piece
+    public PieceList(){
+        super(8);//motel
+    }
 
     public void addIfPiece(Piece p){
         if (p == null || p == Board.edge){
             return;
         }
-        insertBack(p);
+        add(p);
     }
     public void addIfPiece(Piece[] pieces){
         for (Piece p : pieces){
@@ -18,7 +21,7 @@ public class PieceList extends List<Piece>{
     //if P is null, it is not added to the list
     public void addIfColor(Piece p, int color){
         if (p != null && p.color == color){
-            insertBack(p);
+            add(p);
         }
     }
     public void addIfColor(Piece pieces[], int color){
