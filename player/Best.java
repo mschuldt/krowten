693d436daf05b461ee4abf5764dd725ec693012d
Michelle_Chen <cs61b-axx@s271-16.CS.Berkeley.EDU>
2014-03-15 21:17:04 -0700

    added Best.java

---
+++ player/Best.java
@@ -0,0 +1,11 @@
+package player;
+
+public class Best{
+    public int score; //public/private?
+    public Move move;
+    public Best(){
+        score = 0;
+        move = null; // ? not sure
+    }
+}
+
