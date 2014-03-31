a39c655fc1147ef9c3b6537f6d6ba489c8217ff7
mschuldt <mbschuldt@gmail.com>
2014-03-31 01:59:24 -0700

    parameter-less constructor for Piece

--- player/Piece.java
+++ player/Piece.java
@@ -3,9 +3,12 @@ package player;
 public class Piece{
     public int color, x, y;
     public long bitRep; //binary representation
+
+    public Piece(){}
+
     public Piece (int _color, long _bitRep, int _x, int _y){
         color = _color;
-	bitRep = _bitRep;
+        bitRep = _bitRep;
         x = _x;
         y = _y;
     }
