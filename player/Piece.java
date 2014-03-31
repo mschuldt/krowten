dc1c2880c605122bc9af2015065f4b1485962933
mschuldt <mbschuldt@gmail.com>
2014-03-31 02:12:59 -0700

    method for setting piece values

--- player/Piece.java
+++ player/Piece.java
@@ -7,10 +7,15 @@ public class Piece{
     public Piece(){}
 
     public Piece (int _color, long _bitRep, int _x, int _y){
+        set(_color, _bitRep, _x, _y);
+    }
+
+    public Piece set(int _color, long _bitRep, int _x, int _y){
         color = _color;
         bitRep = _bitRep;
         x = _x;
         y = _y;
+        return this;
     }
 
     public String toString(){
