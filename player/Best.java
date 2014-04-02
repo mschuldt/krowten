7c6232ee0bd2daaea4ecca07e00f2d746f93481c
mschuldt <mbschuldt@gmail.com>
2014-04-02 04:42:01 -0700

    sure

--- player/Best.java
+++ player/Best.java
@@ -1,11 +1,10 @@
 package player;
 
 public class Best{
-    public int score; //public/private?
+    public int score;
     public Move move;
     public Best(){
         score = 0;
-        move = null; // ? not sure
+        move = null;
     }
 }
-
