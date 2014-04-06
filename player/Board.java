46dcf23a833692371c741005ed586714f4ec25d6
mschuldt <mbschuldt@gmail.com>
2014-04-05 21:31:28 -0700

    indent

--- player/Board.java
+++ player/Board.java
@@ -1335,13 +1335,13 @@ public class Board{
     //this is used by the evaluation function to determine the
     //values of piece positions
     private int[][] whiteSquareValues = {{ 0, 0, 0, 0,  0, 0, 0, 0},
-                                 {-3,-3, 2,-1,  2, 1, 0,-4},
-                                 {-4,-3,-1,-3, -1,-1, 0,-3},
-                                 {-2,-3, 4,-3,  4, 1, 0,-3},
-                                 {-4,-3,-3,-3, -3,-1, 0,-4},
-                                 {-2,-2, 3,-3,  3, 1, 0,-3},
-                                 {-4,-2, 0,-3,  0,-1, 0,-3},
-                                 {0 , 0, 0, 0,  0, 0, 0, 0}};
+                                         {-3,-3, 2,-1,  2, 1, 0,-4},
+                                         {-4,-3,-1,-3, -1,-1, 0,-3},
+                                         {-2,-3, 4,-3,  4, 1, 0,-3},
+                                         {-4,-3,-3,-3, -3,-1, 0,-4},
+                                         {-2,-2, 3,-3,  3, 1, 0,-3},
+                                         {-4,-2, 0,-3,  0,-1, 0,-3},
+                                         {0 , 0, 0, 0,  0, 0, 0, 0}};
 
     //return the sum of the square values of each piece
     //used by the evaluation function
