3b2ec1db59b7023c3566ff4f6ab1dde6902e8739
mschuldt <mbschuldt@gmail.com>
2014-05-03 16:08:46 -0700

    method: Entry.Entry()

--- dict/Entry.java
+++ dict/Entry.java
@@ -7,6 +7,9 @@ public class Entry {
     public long ourBitBoard, opponentBitBoard;
     public int score;
 
+    public Entry(){
+    }
+
     public Entry(int s, long ourBoard, long opponentBoard){
         score = s;
         ourBitBoard = ourBoard;
