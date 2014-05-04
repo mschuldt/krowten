369d6261caa69062afe6549242f6ebbbfe699875
mschuldt <mbschuldt@gmail.com>
2014-05-03 20:37:55 -0700

    field: Best.evaledBoards

--- player/Best.java
+++ player/Best.java
@@ -3,8 +3,10 @@ package player;
 public class Best{
     public int score;
     public Move move;
+    public int evaledBoards;
     public Best(){
         score = 0;
+        evaledBoards = 1;
         move = null;
     }
 }
